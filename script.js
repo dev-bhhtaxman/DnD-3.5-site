4bcdb99d4359b031f0308a27ec5978def534c7d8
Bilal Husein <dev.bhhtaxman@gmail.com>
2025-10-20 18:47:04 -0500

    updated all

--- script.js
+++ script.js
@@ -27,8 +27,185 @@ function generateOutput() {
   const gender = pickRandom(gender_list);
 
   const result = `${gender} Level ${level} ${selectedClass} with ${eyeColor} eyes, ${hairColor} hair, and feeling ${mood} while usually ${personality}.`;
-  document.getElementById("outputBox").value = result;
+  document.getElementById("outputBox").textContent = result;
 }
 
 // --- connect button ---
 document.getElementById("genChar").addEventListener("click", generateOutput);
+
+
+
+//structure for objects
+
+const human = {
+  Str: '',
+  Dex: '',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: '+1 feat at 1st level, 4 bonus skill points at 1st level, +1 per level thereafter, Favored Class: Any'
+
+}
+
+
+const dwarf = {
+  Str: '+2',
+  Dex: '',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '-2',
+  Notes: 'Darkvision 60 ft. Stonecunning, weapon familiarity (dwarven weapons), stability, +2 vs poison, spells, and spell-like effects. Favored Class: Fighter'
+}
+
+const elf = {
+  Str: '',
+  Dex: '+2',
+  Con: '-2',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Low-light vision, Proficient with longsword, rapier, longbow, and shortbow, Immune to magic sleep; +2 vs enchantments, +2 Listen, Search, Spot. Favored Class: Wizard'
+}
+
+
+const gnome = {
+  Str: '-2',
+  Dex: '',
+  Con: '+2',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Small size, Low-light vision, +2 saving throws vs illusions, Speak with animals (burrowing mammals), +1 to DCs of illusion spells. Favored Class: Bard'
+}
+
+
+const half_elf = {
+  Str: '',
+  Dex: '',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: '+1 Listen, Search, Spot, Immune to sleep spells, +2 Diplomacy, Gather Information, Low-light vision. Favored Class: Any'
+}
+const half_orc = {
+  Str: '+2',
+  Dex: '',
+  Con: '',
+  Int: '-2',
+  Wis: '',
+  Cha: '-2',
+  Notes: 'Darkvision 60 ft. Favored Class: Barbarian'
+}
+const halfling = {
+  Str: '-2',
+  Dex: '+2',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Small size, +1 all saving throws, +2 Climb, Jump, Move Silently. Favored Class: Rogue'
+}
+const goblin = {
+  Str: '',
+  Dex: '',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Small humanoid, +4 Move Silently, Darkvision 60 ft. +1 racial bonus on Ride and Move Silently'
+}
+const orc = {
+  Str: '+4',
+  Dex: '',
+  Con: '',
+  Int: '-2',
+  Wis: '-2',
+  Cha: '-2',
+  Notes: 'Darkvision 60 ft. Light sensitivity'
+}
+const kobold = {
+  Str: '-4',
+  Dex: '+2',
+  Con: '-2',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Darkvision 60 ft. +1 Craft (trapmaking), Profession (miner), Search (stonework)'
+}
+const gnoll = {
+  Str: '+2',
+  Dex: '',
+  Con: '',
+  Int: '-2',
+  Wis: '',
+  Cha: '',
+  Notes: 'Darkvision 60 ft.'
+}
+const lizardfolk = {
+  Str: '+2',
+  Dex: '',
+  Con: '+2',
+  Int: '-2',
+  Wis: '',
+  Cha: '',
+  Notes: 'Natural armor +5, swim speed 15 ft.'
+}
+const merfolk = {
+  Str: '-2',
+  Dex: '+2',
+  Con: '',
+  Int: '',
+  Wis: '',
+  Cha: '',
+  Notes: 'Swim speed 50 ft., low-light vision, Amphibious (can breathe air and water)'
+}
+const troglodyte = {
+  Str: '+4',
+  Dex: '-2',
+  Con: '+4',
+  Int: '-2',
+  Wis: '',
+  Cha: '-2',
+  Notes: 'Darkvision 90 ft., stench aura, Natural armor +6'
+}
+const aasimar = {
+  Str: '',
+  Dex: '',
+  Con: '',
+  Int: '',
+  Wis: '+2',
+  Cha: '+2',
+  Notes: 'Darkvision 60 ft., daylight 1/day, Resistance to acid, cold, electricity 5. Favored Class: Paladin Level Adjust +1'
+}
+const tiefling = {
+  Str: '',
+  Dex: '+2',
+  Con: '',
+  Int: '+2',
+  Wis: '',
+  Cha: '-2',
+  Notes: 'Darkvision 60 ft., darkness 1/day, Resistance to cold, fire, electricity 5. Favored Class: Rogue, Level Adjustment +1'
+}
+const drow = {
+  Str: '',
+  Dex: '+2',
+  Con: '-2',
+  Int: '+2',
+  Wis: '',
+  Cha: '+2',
+  Notes: 'Darkvision 120 ft., spell resistance, light blindness, Spell-like abilities: dancing lights, darkness, faerie fire, Proficient with rapiers, longswords, hand crossbows, Level Adjustment +2'
+}
+const githyanki = {
+  Str: '',
+  Dex: '+2',
+  Con: '',
+  Int: '',
+  Wis: '-2',
+  Cha: '',
+  Notes: 'Darkvision 60 ft. Natural armor +1, Psi-like abilities: daunt, dimension door, psionic teleport (levels vary), Level Adjustment +2'
+}
+
+
