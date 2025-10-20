810d4b4d1b3f733834f2f82bb8419fcfb5397a08
Bilal Husein <dev.bhhtaxman@gmail.com>
2025-10-20 10:56:49 -0500

    updated js

--- script.js
+++ script.js
@@ -1,49 +1,32 @@
+// --- data lists ---
+const class_list = ['Bard', 'Barbarian', 'Wizard', 'Rogue', 'Cleric', 'Paladin', 'Fighter', 'Ranger', 'Druid', 'Monk', 'Sorcerer'];
+const eyecolor_list = ['Blue', 'Gray', 'Brown', 'Black', 'Green', 'Yellow', 'Red', 'Orange', 'White', 'Purple'];
+const haircolor_list = ['Blue', 'Gray', 'Brown', 'Black', 'Green', 'Yellow', 'Red', 'Orange', 'White', 'Purple'];
+const level_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
+const mood_list = ['Angry','Sad','Happy','Afraid','Surprised','Disgusted','Contemptuous','Love','Lustful','Shy','Raging','Trustful','Anticipation','Indifference','Amused','Anxious','In Awe','Interested','Envious','Blah'];
+const personality_list = ['loyal and sensible','responsible and caring','humanitarian and pragmatic','logical and systematic','independent and spontaneous','introspective and creative','quiet and sensitive','curious and intellectual','free spirit','entertainer','innovative and endlessly positive','out of the box thinker and quiet','hard working and pragmatic','personable and go-getting','helper and networker','born leader','angry and spiteful','hateful and jealous','wrathful and vengeful','psychotic and unstable'];
+const quest_list = ['Yes', 'No'];
+const gender_list = ['Male', 'Female', 'Other', 'Nonbinary'];
 
-
-const class_list = ['Bard', 'Barbarian', 'Wizard', 'Rogue', 'Cleric', 'Paladin', 'Fighter', 'Ranger', 'Druid', 'Monk', 'Sorcerer',]
-
-const eyecolor_list = ['Blue', 'Gray', 'Brown', 'Black', 'Green', 'Yellow', 'Red', 'Orange', 'White', 'Purple']
-
-const haircolor_list = ['Blue', 'Gray', 'Brown', 'Black', 'Green', 'Yellow', 'Red', 'Orange', 'White', 'Purple']
-
-const level_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
-
-const mood_list = ['Angry', 'Sad', 'Happy', 'Afraid', 'Surprised', 'Disgusted', 'Contemptuous', 'Love', 'Lustful', 'Shy', 'Raging', 'Trustful', 'Anticipation', 'Indifference', 'Amused', 'Anxious', 'In Awe', 'Interested', 'Envious', 'Blah']
-
-const personality_list = ['loyal and sensible', 'responsible and caring', 'humanitarian and pragmatic', 'logical and systematic', 'independent and spontaneous', 'introspective and creative', 'quiet and sensitive', 'curious and intellectual', 'free spirit', 'entertainer', 'innovative and enlessly positive', 'outof the box thinker and quiet', 'hard working and pragmatic', 'personable and go-getting', 'helper and networker', 'born leader', 'angry and spiteful', 'hateful and jelous', 'wrathful and vengeful', 'psychotic and unstable']
-
-const quest_list = ['Yes', 'No']
-
-const gender_list = ['Male', 'Female', 'Other', 'Nonbinary']
-
-const race_list = ['Human', 'Dwarf', 'Elf', 'Gnome', 'Half-elf', 'Half-orc', 'Halfling', 'Goblin', 'Orc', 'Kobold', 'Gnoll', 'Lizardfolk', 'Merfolk', 'Troglodyte', 'Aasimar', 'Tiefling', 'Drow', 'Githyanki']
-
-const Map = {'Human' : 'Stats',}
-
-
-function generateOutput() {
-   let result = "";
-   for (let i = 0; i < 1; i++) {
-      const class_list = class_list[Math.floor(Math.random() * class_list.length)];
-      const eyecolor_list = eyecolor_list[Math.floor(Math.random() * eyecolor_list.length)];
-      const haircolor_list = haircolor_list[Math.floor(Math.random() * haircolor_list.length)];
-      const level_list = level_list[Math.floor(Math.random() * level_list.length)];
-      const mood_list = mood_list[Math.floor(Math.random() * mood_list.length)];
-      const personality_list = personality_list[Math.floor(Math.random() * personality_list.length)];
-      const quest_list = quest_list[Math.floor(Math.random() * quest_list.length)];
-      const gender_list = gender_list[Math.floor(Math.random() * gender_list.length)];
-      
-      
-      result += `${class_list} ${eyecolor_list} ${haircolor_list} ${level_list} ${mood_list} ${personality_list} ${quest_list} ${gender_list}\n`;
-   }
-   document.getElementById("outputBox").value = result;
+// --- helper function ---
+function pickRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
 }
+
+// --- main generator ---
+function generateOutput() {
+  const selectedClass = pickRandom(class_list);
+  const eyeColor = pickRandom(eyecolor_list);
+  const hairColor = pickRandom(haircolor_list);
+  const level = pickRandom(level_list);
+  const mood = pickRandom(mood_list);
+  const personality = pickRandom(personality_list);
+  const quest = pickRandom(quest_list);
+  const gender = pickRandom(gender_list);
+
+  const result = `${selectedClass} ${eyeColor} ${hairColor} ${level} ${mood} ${personality} ${quest} ${gender}`;
+  document.getElementById("outputBox").value = result;
+}
+
+// --- connect button ---
 document.getElementById("genChar").addEventListener("click", generateOutput);
-
-
-
-
-
-
-
-
