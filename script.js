642ac5b906a51a77da2d607568a00e075ce7a1d2
Bilal Husein <dev.bhhtaxman@gmail.com>
2025-10-20 13:43:51 -0500

    added js, fixed js, fixing css and some html

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ const haircolor_list = ['Blue', 'Gray', 'Brown', 'Black', 'Green', 'Yellow', 'Re
 const level_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'];
 const mood_list = ['Angry','Sad','Happy','Afraid','Surprised','Disgusted','Contemptuous','Love','Lustful','Shy','Raging','Trustful','Anticipation','Indifference','Amused','Anxious','In Awe','Interested','Envious','Blah'];
 const personality_list = ['loyal and sensible','responsible and caring','humanitarian and pragmatic','logical and systematic','independent and spontaneous','introspective and creative','quiet and sensitive','curious and intellectual','free spirit','entertainer','innovative and endlessly positive','out of the box thinker and quiet','hard working and pragmatic','personable and go-getting','helper and networker','born leader','angry and spiteful','hateful and jealous','wrathful and vengeful','psychotic and unstable'];
-const quest_list = ['Yes', 'No'];
-const gender_list = ['Male', 'Female', 'Other', 'Nonbinary'];
+const gender_list = ['Male', 'Female', 'Nonbinary'];
+
+
 
 // --- helper function ---
 function pickRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
+
 
 // --- main generator ---
 function generateOutput() {
@@ -21,10 +24,9 @@ function generateOutput() {
   const level = pickRandom(level_list);
   const mood = pickRandom(mood_list);
   const personality = pickRandom(personality_list);
-  const quest = pickRandom(quest_list);
   const gender = pickRandom(gender_list);
 
-  const result = `${selectedClass} ${eyeColor} ${hairColor} ${level} ${mood} ${personality} ${quest} ${gender}`;
+  const result = `${gender} Level ${level} ${selectedClass} with ${eyeColor} eyes, ${hairColor} hair, and feeling ${mood} while usually ${personality}.`;
   document.getElementById("outputBox").value = result;
 }
 
